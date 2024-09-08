0f1d84a408521bdad94193dc65bdf6d075906126
Samuel Okoro Chka <okorosamuelchika@gmail.com>
2024-09-08 13:56:30 +0100

    fix responsiveness

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -52,14 +52,14 @@ export default function Home() {
 
   return (
     <main className="flex flex-col md:flex-row">
-      <div className="md:w-2/5 hidden md:block h-full bg-gradient-to-b from-[#93AFC9] to-cyan-500 px-5 py-2">
+      <div className="md:w-2/5 hidden md:block bg-gradient-to-b from-[#93AFC9] to-cyan-500 px-5 py-2">
         <h1 className="text-center m-3 font-bold text-xl">
           Top 10 Web Application Security Risks
         </h1>
         <OwaspTenAccordion />
       </div>
-      <div className="md:w-3/5 w-full md:h-screen flex flex-col p-5 gap-10">
-        <div className="flex flex-col items-start gap-3 mt-20">
+      <div className="md:w-3/5 w-full md:h-screen flex flex-col md:my-12 p-5 gap-10">
+        <div className="flex flex-col items-start md:items-center gap-3 mt-20">
           <div className="md:text-5xl text-4xl text-center font-extrabold text-gray-900 mb-4">
             Visualize your Codebase
           </div>
